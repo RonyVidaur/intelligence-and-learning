ddc97b6c0f53b9fda781af51ef33f756b56b31da
Rony Vidaur <rony.vidaur@gmail.com>
2017-03-31 10:27:59 -0600

    basic binary tree structure

---
+++ main.js
@@ -0,0 +1,46 @@
+var tree
+
+function setup() {
+  noCanvas()
+  tree = new Tree()
+  tree.addValue(5)
+  tree.addValue(3)
+  tree.addValue(7)
+  tree.addValue(6)
+  console.log(tree)
+}
+
+function Tree() {
+  this.root = null
+}
+
+function Node(val) {
+  this.value = val
+  this.left = null
+  this.right = null
+}
+
+Node.prototype.addNode = function(n) {
+  if (n.value < this.value) {
+    if (this.left == null) {
+      this.left = n
+    } else {
+      this.left.addNode(n)
+    }
+  } else if (n.value > this.value){
+      if(this.right == null) {
+        this.right = n
+      } else {
+        this.right.addNode(n)
+      }
+  }
+}
+
+Tree.prototype.addValue = function(val) {
+  var n = new Node(val)
+  if (this.root === null) {
+    this.root = n
+  } else {
+    this.root.addNode(n)
+  }
+}
